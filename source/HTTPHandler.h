8b8147c269e61e13ca911ffba708efdc25af5948
JojoS62 <sn2010@onlinehome.de>
2020-01-07 17:24:18 +0100

    resolved circular reference

--- source/HTTPHandler.h
+++ source/HTTPHandler.h
@@ -1,7 +1,10 @@
 #ifndef __HTTP_Handler__
 #define __HTTP_Handler__
 
-#include "ClientConnection.h"
+#include <map>
+
+class HttpParsedRequest;
+class ClientConnection;
 
 typedef Callback<void(HttpParsedRequest*, ClientConnection* )> CallbackRequestHandler;
 typedef std::map<std::string, CallbackRequestHandler> HTTPSocketHandlerContainer;
